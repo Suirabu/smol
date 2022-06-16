f6cf3f0499f6488c3b0dd1e31ceef2f9dff45f12
Suirabu <suirabu.dev@gmail.com>
2022-06-15 23:34:41 -0700

    Refactor `build.zig` to allow manual creation of executables
    The previous method of adding executables to our builder using a
    helper function prevented us from being able to modify the build steps
    of said executable in a neat manner. For this reason the helper function
    was removed.
    
    A new helper function `addRunStep` was added in its place to easily add
    a run step to a given executable.

--- build.zig
+++ build.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
 
-pub fn addExecutableWithStep(b: *std.build.Builder, comptime suffix: []const u8, root_src: ?[]const u8) void {
+pub fn addRunStep(b: *std.build.Builder, exe: *std.build.LibExeObjStep, comptime suffix: []const u8) void {
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(&exe.step);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const exe_name = "smol-" ++ suffix;
+    const run_step = b.step("run-" ++ suffix, "Run " ++ exe_name);
+    run_step.dependOn(&run_cmd.step);
+}
+
+pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -11,22 +23,10 @@ pub fn addExecutableWithStep(b: *std.build.Builder, comptime suffix: []const u8,
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe_name = "smol-" ++ suffix;
-    const exe = b.addExecutable(exe_name, root_src);
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
-    
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(&exe.step);
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    const smol_emu = b.addExecutable("smol-emu", "src/emulator/main.zig");
+    smol_emu.setTarget(target);
+    smol_emu.setBuildMode(mode);
+    smol_emu.install();
 
-    const run_step = b.step("run-" ++ suffix, "Run " ++ exe_name);
-    run_step.dependOn(&run_cmd.step);
-}
-
-pub fn build(b: *std.build.Builder) void {
-    addExecutableWithStep(b, "emu", "src/emulator/main.zig");
+    addRunStep(b, smol_emu, "emu");
 }
